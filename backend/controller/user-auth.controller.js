9199af7faece253e21747767649bf7f75163f95b
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-20 20:06:53 +0800

    feat: Created the basic user creation. wip: Hash the password

--- backend/controller/user-auth.controller.js
+++ backend/controller/user-auth.controller.js
@@ -1,1 +1,17 @@
 import User from "../models/user.model";
+
+export const signUp = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ success: false, message: "User has an existing account." });
+        };
+        const newUser = new User({ email, password });
+        await newUser.save();
+        return res.status(201).json({ success: true, message: "User registered successfully." });
+    } catch (error) {
+        console.error("Error in Signin", error);
+        return res.status(500).json({ success: false, message: `Error Signing in: ${error}` });
+    };
+};
