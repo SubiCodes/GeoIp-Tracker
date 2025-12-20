00a4f79db05ac9dc2b6fea2fd23bc85568269518
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-21 00:02:13 +0800

    feat: Installed the taost

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import 'leaflet/dist/leaflet.css';
+import { Toaster } from "@/components/ui/sonner"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Toaster />
   </StrictMode>,
 )
