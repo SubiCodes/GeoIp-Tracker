dea8fe46c599af6bb2458b74af79568764f1871a
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-20 22:28:48 +0800

    feat: Install the router deps

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AuthPages from './layouts/AuthPages';
+// import SignIn from './pages/SignIn'; // You will add this page later
 import { Button } from './components/ui/button'
 
 function App() {
-
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <AuthPages>
+              {/* Replace with <SignIn /> when you create the page */}
+              <div>SignIn Page Placeholder</div>
+            </AuthPages>
+          }
+        />
+      </Routes>
       <Button className='bg-red-500'>Hello World</Button>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
