ab9362729477129e7e284b974fe3833cd65ad814
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-20 20:20:54 +0800

    feat: Implemented cookie parser

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import { connectDB } from './config/db.js';
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
     origin: [(process.env.FRONTEND_URI || 'http://localhost:5173'), 'http://localhost:3000'],
     credentials: true
