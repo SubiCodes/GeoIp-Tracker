02bc47209b151ce5b7985e3690adee33722ccc03
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-21 00:01:19 +0800

    feat: Have the axios on the store

--- frontend/src/store/user-authStore.tsx
+++ frontend/src/store/user-authStore.tsx
@@ -1,4 +1,10 @@
 import { create } from 'zustand'
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL ||'http://localhost:3000/',
+    withCredentials: true,
+});
 
 export interface User {
     userId: string
