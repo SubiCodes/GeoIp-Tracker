ca85a43d566a1c8c1a8ade788708fe698798c185
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-20 22:54:57 +0800

    feat: Display the new pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom
 import AuthLayout from './layouts/AuthLayout';
 import MainLayout from './layouts/MainLayout';
 import SignIn from './pages/auth/SignIn';
+import SignUp from './pages/auth/SignUp';
 // import SignIn from './pages/SignIn';
 // import SignUp from './pages/SignUp';
 
@@ -12,7 +13,7 @@ function App() {
 
         <Route element={<AuthLayout><Outlet /></AuthLayout>}>
           <Route path="/" element={<SignIn />} />
-          <Route path="/signup" element={<div>SignUp Page Placeholder</div>} />
+          <Route path="/signup" element={<SignUp />} />
         </Route>
 
         <Route element={<MainLayout><Outlet /></MainLayout>}>
