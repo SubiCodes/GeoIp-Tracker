fcb1c4be7f08a535a52d0cc0383a0730c5f27a4d
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-21 22:45:34 +0800

    feat: Added a base file for the Adding of IP address

---
+++ frontend/src/components/dialogs/AddSaveIPDialog.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from "@/components/ui/alert-dialog"
+
+
+interface AddSaveIPDialogProps {
+    open: boolean;
+    onOpenChange?: (open: boolean) => void;
+}
+
+const AddSaveIPDialog: React.FC<AddSaveIPDialogProps> = ({ open, onOpenChange }) => {
+    return (
+        <AlertDialog open={open} onOpenChange={onOpenChange}>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle className='text-red-500'>Save a new IP Address</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        Enter the IP address you want to save to your list.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                    <AlertDialogAction>Continue</AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+};
+
+export default AddSaveIPDialog
