74e41e0c31b4a55da70cb8f09ae6d94d75da5b30
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-22 03:24:36 +0800

    feat: Created the add history item function

--- frontend/src/store/ipHistoryStore.tsx
+++ frontend/src/store/ipHistoryStore.tsx
@@ -1,6 +1,7 @@
 import { create } from 'zustand'
 import axios from 'axios'
 import type { IPGeoData } from './ipgeoStore';
+import { toast } from 'sonner';
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/',
@@ -12,6 +13,7 @@ interface IPHistoryStoreState {
     fetchingIPHistory: boolean;
     fetchingIPHistoryError?: string | null;
     fetchIPHistory: () => Promise<void>;
+    addHistoryItem: (ip: string) => void;
 }
 
 export const useIPHistoryStore = create<IPHistoryStoreState>((set) => ({
@@ -43,5 +45,28 @@ export const useIPHistoryStore = create<IPHistoryStoreState>((set) => ({
         } finally {
             set({ fetchingIPHistory: false });
         }
-    }
+    },
+    addHistoryItem: async (ip: string) => {
+        try {
+            toast.loading("Adding IP to history...");
+            const res = await api.post('/ip-history', { ip });
+            set((state) => ({ ipHistory: [...state.ipHistory, res.data] }));
+            toast.success("New IP set as current IP and added to history!");
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                const msg = error.response?.data?.message;
+                if (msg?.title && msg?.suggestion) {
+                    toast.error(`${msg.title}: ${msg.suggestion}`);
+                } else if (msg?.title) {
+                    toast.error(msg.title);
+                } else if (msg?.suggestion) {
+                    toast.error(msg.suggestion);
+                } else {
+                    toast.error("Fetching IP History failed");
+                }
+            } else {
+                toast.error((error as Error).message || "Fetching IP History failed");
+            }
+        }
+    },
 }));
