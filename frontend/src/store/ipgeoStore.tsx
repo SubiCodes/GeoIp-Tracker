e2b22d2a1cea555acd0764b1711b6ca3444bc0b7
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-22 01:57:44 +0800

    feat: Created the store for displaying the geo data provided the IP

--- frontend/src/store/ipgeoStore.tsx
+++ frontend/src/store/ipgeoStore.tsx
@@ -49,136 +49,38 @@ export interface IPGeoData {
 };
 
 interface IPGeoStoreState {
-    currentIPGeo: IPGeoData | null,
-    fetchingCurrentIPGeo: boolean,
-    fetchCurrentIPGeo: () => Promise<void>,
-    fetcingCurrentIPGeoError?: string | null,
-    ipGeoDatas: IPGeoData[] | null,
-    fetchingIPGeoDatas: boolean,
-    fetchIPGeoDatas: () => Promise<void>,
-    fetcingIPGeoDatasError?: string | null,
-    addingIPGeoData: boolean,
-    addIPGeoData: (ip: string, description?: string) => Promise<string|void>,
-    addingIPGeoDataError?: string | null,
-    deletingIPGeoData: string[],
-    deleteIPGeoData: (id: string) => Promise<boolean>,
+    displayedIPGeoData: IPGeoData | null;
+    displayingIPGeoData: boolean;
+    displayingIPGeoDataError?: string | null;
+    displayIPGeoData: (ip?: string) => Promise<void>;
 };
 
 export const useIPGeoStore = create<IPGeoStoreState>((set) => ({
-    currentIPGeo: null,
-    fetchingCurrentIPGeo: false,
-    fetchCurrentIPGeo: async () => {
-        set({ fetchingCurrentIPGeo: true, fetcingCurrentIPGeoError: null });
+    displayedIPGeoData: null,
+    displayingIPGeoData: false,
+    displayingIPGeoDataError: null,
+    displayIPGeoData: async (ip?: string) => {
+        set({ displayingIPGeoData: true, displayingIPGeoDataError: null, displayedIPGeoData: null });
         try {
-            const res = await axios.get('https://ipwho.is/');
-            if (res.data && res.data.success) {
-                set({ currentIPGeo: res.data });
-            };
+            const res = await axios.get(`https://ipwho.is/${ip || ''}`);
+            set({ displayedIPGeoData: res.data });
         } catch (error) {
-            set({ currentIPGeo: null });
             if (axios.isAxiosError(error)) {
                 const msg = error.response?.data?.message;
                 if (msg?.title && msg?.suggestion) {
-                    set({ fetcingCurrentIPGeoError: `${msg.title}: ${msg.suggestion}` });
+                    set({ displayingIPGeoDataError: `${msg.title}: ${msg.suggestion}` });
                 } else if (msg?.title) {
-                    set({ fetcingCurrentIPGeoError: msg.title });
+                    set({ displayingIPGeoDataError: msg.title });
                 } else if (msg?.suggestion) {
-                    set({ fetcingCurrentIPGeoError: msg.suggestion });
+                    set({ displayingIPGeoDataError: msg.suggestion });
                 } else {
-                    set({ fetcingCurrentIPGeoError: "Unable to get your current IP geolocation." });
+                    set({ displayingIPGeoDataError: "Displaying IP Geo Data failed" });
                 }
             } else {
-                set({ fetcingCurrentIPGeoError: (error as Error).message || "Unable to get your current IP geolocation." });
+                set({ displayingIPGeoDataError: (error as Error).message || "Displaying IP Geo Data failed" });
             }
         } finally {
-            set({ fetchingCurrentIPGeo: false });
-        }
-    },
-    fetcingCurrentIPGeoError: null,
-    ipGeoDatas: null,
-    fetchingIPGeoDatas: false,
-    fetchIPGeoDatas: async () => {
-        set({ fetchingIPGeoDatas: true, fetcingIPGeoDatasError: null, ipGeoDatas: null });
-        try {
-            const res = await api.get('/ipgeo');
-            if (res.data && res.data.success && res.data.data) {
-                set({ ipGeoDatas: res.data.data });
-            };
-        } catch (error) {
-            set({ currentIPGeo: null });
-            if (axios.isAxiosError(error)) {
-                const msg = error.response?.data?.message;
-                if (msg?.title && msg?.suggestion) {
-                    set({ fetcingIPGeoDatasError: `${msg.title}: ${msg.suggestion}` });
-                } else if (msg?.title) {
-                    set({ fetcingIPGeoDatasError: msg.title });
-                } else if (msg?.suggestion) {
-                    set({ fetcingIPGeoDatasError: msg.suggestion });
-                } else {
-                    set({ fetcingIPGeoDatasError: "Unable to get your saved IP geolocations." });
-                }
-            } else {
-                set({ fetcingIPGeoDatasError: (error as Error).message || "Unable to get your saved IP geolocations." });
-            }
-        } finally {
-            set({ fetchingIPGeoDatas: false });
-        }
-    },
-    fetcingIPGeoDatasError: null,
-    addingIPGeoData: false,
-    addIPGeoData: async (ip: string, description?: string) => {
-        set({ addingIPGeoData: true, addingIPGeoDataError: null });
-        try {
-            const res = await api.post('/ipgeo', { ip, description });
-            if (res.data && res.data.success && res.data.data) {
-                set((state) => ({
-                    ipGeoDatas: state.ipGeoDatas ? [...state.ipGeoDatas, res.data.data] : [res.data.data]
-                }));
-            }
-        } catch (error) {
-            set({ currentIPGeo: null });
-            if (axios.isAxiosError(error)) {
-                const msg = error.response?.data?.message;
-                if (msg?.title && msg?.suggestion) {
-                    set({ addingIPGeoDataError: `${msg.title}: ${msg.suggestion}` });
-                } else if (msg?.title) {
-                    set({ addingIPGeoDataError: msg.title });
-                } else if (msg?.suggestion) {
-                    set({ addingIPGeoDataError: msg.suggestion });
-                } else {
-                    set({ addingIPGeoDataError: "Unable to save IP geolocation data." });
-                }
-                return msg?.suggestion || "Unable to save IP geolocation data.";
-            } else {
-                set({ addingIPGeoDataError: (error as Error).message || "Unable to save IP geolocation data." });
-                return "Unable to save IP geolocation data.";
-            }
-        } finally {
-            set({ addingIPGeoData: false });
-        }
-    },
-    addingIPGeoDataError: null,
-    deletingIPGeoData: [],
-    deleteIPGeoData: async (id: string) => {
-        set((state) => {
-            if (state.deletingIPGeoData.includes(id)) return state;
-            return { deletingIPGeoData: [...state.deletingIPGeoData, id] };
-        });
-        try {
-            const res = await api.delete(`/ipgeo/${id}`);
-            if (res.data && res.data.success) {
-                set((state) => ({
-                    ipGeoDatas: state.ipGeoDatas ? state.ipGeoDatas.filter((item) => item._id !== id) : null
-                }));
-                return true;
-            } else {
-                return false;
-            }
-        } catch (error) {
-            console.error("Error deleting IP geolocation data:", error);
-            return false
-        } finally {
-            set((state) => ({ deletingIPGeoData: state.deletingIPGeoData.filter((item) => item !== id) }));
+            set({ displayingIPGeoData: false });
         }
     }
 }));
