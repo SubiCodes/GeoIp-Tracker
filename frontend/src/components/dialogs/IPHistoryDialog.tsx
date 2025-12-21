fbc1559f79f896b34ab80f235a7f0913b0ae7779
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-22 03:20:16 +0800

    feat: Display the recent ips

--- frontend/src/components/dialogs/IPHistoryDialog.tsx
+++ frontend/src/components/dialogs/IPHistoryDialog.tsx
@@ -1,19 +1,23 @@
 import {
     AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
     AlertDialogContent,
-    AlertDialogFooter,
     AlertDialogHeader,
     AlertDialogTitle,
     AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Label } from "@/components/ui/label"
+import { useIPHistoryStore } from "@/store/ipHistoryStore";
+import AddedIPCard from "../cards/AddedIPCard";
 
 interface IPHistoryDialogProps {
     triggerButton: React.ReactNode;
 }
 
 function IPHistoryDialog({ triggerButton }: IPHistoryDialogProps) {
+
+    const ipHistory = useIPHistoryStore((state) => state.ipHistory);
+
     return (
         <AlertDialog>
             <AlertDialogTrigger >{triggerButton}</AlertDialogTrigger>
@@ -21,10 +25,31 @@ function IPHistoryDialog({ triggerButton }: IPHistoryDialogProps) {
                 <AlertDialogHeader className="px-6 pt-6 pb-4 border-b">
                     <AlertDialogTitle className="text-xl font-semibold">IP Addresses History</AlertDialogTitle>
                 </AlertDialogHeader>
-                <AlertDialogFooter>
-                    <AlertDialogCancel>Cancel</AlertDialogCancel>
-                    <AlertDialogAction>Continue</AlertDialogAction>
-                </AlertDialogFooter>
+                <div className="flex-1 flex flex-col">
+                    {/* HEADER */}
+                    <div>
+
+                    </div>
+                    <div className="flex flex-col">
+                        {ipHistory.length === 0 && (
+                            <div className="p-6 text-center text-muted-foreground">
+                                No IP history available. Displaying current your IP geolocation.
+                            </div>
+                        )}
+                        {ipHistory.length > 0 && (
+                            ipHistory.map((ipgeo) => (
+                                <Label id={`label-${ipgeo._id}`} className="hover:bg-accent/50 flex items-start gap-3 rounded-lg border p-3 has-[[aria-checked=true]]:border-blue-600 has-[[aria-checked=true]]:bg-blue-50 dark:has-[[aria-checked=true]]:border-blue-900 dark:has-[[aria-checked=true]]:bg-blue-950">
+                                    <Checkbox
+                                        id={`toggle-${ipgeo._id}`}
+                                        defaultChecked
+                                        className="data-[state=checked]:border-blue-600 data-[state=checked]:bg-blue-600 data-[state=checked]:text-white dark:data-[state=checked]:border-blue-700 dark:data-[state=checked]:bg-blue-700"
+                                    />
+                                    <AddedIPCard key={ipgeo._id} data={ipgeo} />
+                                </Label>
+                            ))
+                        )}
+                    </div>
+                </div>
             </AlertDialogContent>
         </AlertDialog>
     )
