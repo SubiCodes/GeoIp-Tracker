b636c5d150e8dc55fcc98614016d664b2e2a618e
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-21 23:20:27 +0800

    feat: Removed Unnecesarry imports

--- frontend/src/components/dialogs/AddSaveIPDialog.tsx
+++ frontend/src/components/dialogs/AddSaveIPDialog.tsx
@@ -4,8 +4,6 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import {
     AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
     AlertDialogContent,
     AlertDialogDescription,
     AlertDialogFooter,
