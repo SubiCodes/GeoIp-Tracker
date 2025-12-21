2027516d690c03fc6626d3643c862d3a8916beba
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-21 21:10:46 +0800

    feat: Created a route for deleting a saved IP address

--- backend/route/ipgeo.route.js
+++ backend/route/ipgeo.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createIpGeo, getUserIpGeos, updateIpGeo } from "../controller/ipgeo.controller.js";
+import { createIpGeo, deleteIpGeo, getUserIpGeos, updateIpGeo } from "../controller/ipgeo.controller.js";
 
 const ipgeoRouter = Router();
 
 ipgeoRouter.post('/', createIpGeo);
 ipgeoRouter.get('/', getUserIpGeos);
 ipgeoRouter.put('/', updateIpGeo);
+ipgeoRouter.delete('/', deleteIpGeo);
 
 export default ipgeoRouter;
