ecaeff8997b1f34fec0cfa8bba90f0fe3a685cb0
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-21 20:58:49 +0800

    fix: Fixed the route for editing the geoip

--- backend/route/ipgeo.route.js
+++ backend/route/ipgeo.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createIpGeo, getUserIpGeos } from "../controller/ipgeo.controller.js";
+import { createIpGeo, getUserIpGeos, updateIpGeo } from "../controller/ipgeo.controller.js";
 
 const ipgeoRouter = Router();
 
 ipgeoRouter.post('/', createIpGeo);
 ipgeoRouter.get('/', getUserIpGeos);
+ipgeoRouter.put('/', updateIpGeo);
 
 export default ipgeoRouter;
