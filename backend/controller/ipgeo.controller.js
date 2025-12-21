d64c2e921ba6cb76810c27c932ac7548c0318a97
Rein Subisol <subisolreineerkim@gmail.com>
2025-12-21 19:15:36 +0800

    feat: Created the base for the IP Geo controller

---
+++ backend/controller/ipgeo.controller.js
@@ -0,0 +1,19 @@
+import User from "../models/user.model.js";
+import IPGeo from "../models/ipgeo.model.js";
+import { v4 as uuidv4 } from 'uuid';
+
+export const createIpGeo = async (req, res) => {
+
+};
+
+export const getUserIpGeos = async (req, res) => {
+
+};
+
+export const updateIpGeo = async (req, res) => {
+
+};
+
+export const deleteIpGeo = async (req, res) => {
+
+};
